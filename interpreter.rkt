715bd3238a95a797c390fde1437c6b0342d67d1e
ulisess1 <usoto1016@gmail.com>
2024-02-17 13:16:43 -0500

    add M_boolean

--- interpreter.rkt
+++ interpreter.rkt
@@ -4,8 +4,28 @@
 
 (define interpret
   (lambda (filename)
-    (let ((syntax-tree (parser filename)))
-      syntax-tree)))
+    (interpret-rec (parser filename))))
+
+(define interpret-rec
+  (lambda (syntax-tree)
+    (cond
+      [(null? syntax-tree) '()]
+      [else '()])))
+
+(define M_boolean
+  (lambda (exp)
+    (cond
+      [(eq? exp 'true) #t]
+      ((eq? exp 'false) #f)
+      ((eq? (operator exp) '==) (eq? (M_integer (leftoperand exp)) (M_integer (rightoperand exp))))
+      [(eq? (operator exp) '!=) (not (M_integer (leftoperand exp)) (M_integer (rightoperand exp)))]
+      [(eq? (operator exp) '<)  (<   (M_integer (leftoperand exp)) (M_integer (rightoperand exp)))]
+      [(eq? (operator exp) '>)  (>   (M_integer (leftoperand exp)) (M_integer (rightoperand exp)))]
+      [(eq? (operator exp) '<=) (<=  (M_integer (leftoperand exp)) (M_integer (rightoperand exp)))]
+      [(eq? (operator exp) '>=) (>=  (M_integer (leftoperand exp)) (M_integer (rightoperand exp)))]
+      [(eq? (operator exp) '&&) (eq? (M_boolean (leftoperand exp)) (M_boolean (rightoperand exp)))]
+      [(eq? (operator exp) '||) (or  (M_boolean (leftoperand exp)) (M_boolean (rightoperand exp)))]
+      [(eq? (operator exp) '!)  (not (M_boolean (leftoperand exp)))])))
 
 (define M_integer
   (lambda (exp)
@@ -17,21 +37,6 @@
       [(eq? (operator exp) '/) (quotient (M_integer (leftoperand exp)) (M_integer (rightoperand exp)))]
       [(eq? (operator exp) '%) (remainder (M_integer (leftoperand exp)) (M_integer (rightoperand exp)))])))
 
-(define operator car)
+(define operator (lambda (exp) (car exp)))
 (define leftoperand cadr)
 (define rightoperand caddr)
-
-
-(define addBinding
-  (lambda (state var value)
-    (cons (addBinding-rec (car state) var) (cons (addBinding-rec (car (cdr state)) value) '()))))
-
-(define addBinding-rec
-  (lambda (ls value)
-    (if (null? ls)
-        (cons value '())
-        (cons (car ls) (addBinding-rec (cdr ls) value)))))
-      
-    
-
-
