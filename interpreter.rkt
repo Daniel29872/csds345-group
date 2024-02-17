d5ea5e1a9a77b03586097f7ee52d0fe35add5ce0
jimmyngo4 <105337446+jimmyngo4@users.noreply.github.com>
2024-02-17 13:11:58 -0500

    updated interpret

--- interpreter.rkt
+++ interpreter.rkt
@@ -4,13 +4,8 @@
 
 (define interpret
   (lambda (filename)
-    (interpret-rec (parser filename))))
-
-(define interpret-rec
-  (lambda (syntax-tree)
-    (cond
-      [(null? syntax-tree) '()]
-      [else '()])))
+    (let ((syntax-tree (parser filename)))
+      syntax-tree)))
 
 (define M_integer
   (lambda (exp)
@@ -22,6 +17,6 @@
       [(eq? (operator exp) '/) (quotient (M_integer (leftoperand exp)) (M_integer (rightoperand exp)))]
       [(eq? (operator exp) '%) (remainder (M_integer (leftoperand exp)) (M_integer (rightoperand exp)))])))
 
-(define operator (lambda (exp) (car exp)))
+(define operator car)
 (define leftoperand cadr)
 (define rightoperand caddr)
