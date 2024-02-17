9e7151a2b8eb29e4b8de5094310fbe0a05fa0c02
Daniel29872 <arnholtdaniel@gmail.com>
2024-02-17 12:45:38 -0500

    add M_integer code from class

--- interpreter.rkt
+++ interpreter.rkt
@@ -11,3 +11,17 @@
     (cond
       [(null? syntax-tree) '()]
       [else '()])))
+
+(define M_integer
+  (lambda (exp)
+    (cond
+      [(number? exp) exp]
+      [(eq? (operator exp) '+) (+ (M_integer (leftoperand exp)) (M_integer (rightoperand exp)))]
+      [(eq? (operator exp) '-) (- (M_integer (leftoperand exp)) (M_integer (rightoperand exp)))]
+      [(eq? (operator exp) '*) (* (M_integer (leftoperand exp)) (M_integer (rightoperand exp)))]
+      [(eq? (operator exp) '/) (quotient (M_integer (leftoperand exp)) (M_integer (rightoperand exp)))]
+      [(eq? (operator exp) '%) (remainder (M_integer (leftoperand exp)) (M_integer (rightoperand exp)))])))
+
+(define operator (lambda (exp) (car exp)))
+(define leftoperand cadr)
+(define rightoperand caddr)
